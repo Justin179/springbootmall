00cf05cd55ad3aade081c617b3fb3b1e88831f58
陳君豪 <justin.uob@gmail.com>
2023-10-11 16:29:43 +0800

    實作修改商品

--- src/main/java/com/justin/springbootmall/controller/ProductController.java
+++ src/main/java/com/justin/springbootmall/controller/ProductController.java
@@ -10,8 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.HttpRetryException;
 
 @RestController
 public class ProductController {
@@ -36,6 +39,19 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+        // 確認商品存在
+        Product productById = productService.getProductById(productId);
+        if (productById==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        // 才進行該商品的update
+        productService.updateProduct(productId,productRequest);
+        Product product = productService.getProductById(productId);
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
 
 
 
