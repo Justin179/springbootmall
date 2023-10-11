bd4b1c352412500354b21228ed263524582557ff
陳君豪 <justin.uob@gmail.com>
2023-10-11 15:52:56 +0800

    實作新增商品

--- src/main/java/com/justin/springbootmall/dao/ProductDao.java
+++ src/main/java/com/justin/springbootmall/dao/ProductDao.java
@@ -1,9 +1,11 @@
 package com.justin.springbootmall.dao;
 
+import com.justin.springbootmall.dto.ProductRequest;
 import com.justin.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
 
+    Integer createProduct(ProductRequest productRequest);
 }
