fcb530bb6878eab1e3e8a7639bdf71d1874e4f80
陳君豪 <justin.uob@gmail.com>
2023-10-13 23:49:38 +0800

    訂單 - 創建訂單 下

--- src/main/java/com/justin/springbootmall/service/OrderService.java
+++ src/main/java/com/justin/springbootmall/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.justin.springbootmall.service;
 
 import com.justin.springbootmall.dto.CreateOrderRequest;
+import com.justin.springbootmall.model.Order;
 
 public interface OrderService {
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
+
+    Order getOrderById(Integer orderId);
 }
