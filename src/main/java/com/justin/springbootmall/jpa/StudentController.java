352ec1d212dea380cf136c1891f7c0008702cc1c
陳君豪 <justin.uob@gmail.com>
2023-10-24 12:12:59 +0800

    jpa CRUD

--- src/main/java/com/justin/springbootmall/jpa/StudentController.java
+++ src/main/java/com/justin/springbootmall/jpa/StudentController.java
@@ -1,7 +1,11 @@
 package com.justin.springbootmall.jpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +16,55 @@ public class StudentController {
     private StudentRepository studentRepository;
 
     @PostMapping("/students")
-    public String insert(@RequestBody Student student){
+    public String create(@RequestBody Student student){
         studentRepository.save(student);
-        return "insert student success";
+        return "create student success";
     }
+
+    @GetMapping("/students/{studentId}")
+    public Student read(@PathVariable Long studentId){
+        Student student = studentRepository.findById(studentId).orElse(null);
+        return student;
+    }
+
+    @PutMapping("/students/{studentId}")
+    public String update(@PathVariable Long studentId, @RequestBody Student student){
+
+        Student existingStudent = studentRepository.findById(studentId).orElse(null);
+        if (existingStudent !=null){
+            existingStudent.setName(student.getName());
+            existingStudent.setAge(student.getAge());
+            existingStudent.setEmail(student.getEmail());
+            studentRepository.save(existingStudent);
+            return "update student success"; // insert if not exist
+        }
+
+        return "student id: " + studentId + "doesn't exist";
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public String delete(@PathVariable Long studentId){
+
+        studentRepository.deleteById(studentId);
+        return "delete student success";
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
