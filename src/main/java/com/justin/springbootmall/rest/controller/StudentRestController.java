4c4699babb8cdb7a183be9f78f2afb44955174e3
陳君豪 <justin.uob@gmail.com>
2023-10-28 22:03:03 +0800

    define own exception and let spring handle the exception

--- src/main/java/com/justin/springbootmall/rest/controller/StudentRestController.java
+++ src/main/java/com/justin/springbootmall/rest/controller/StudentRestController.java
@@ -2,8 +2,13 @@ package com.justin.springbootmall.rest.controller;
 
 import com.justin.springbootmall.jpa.relationship.entity.Student;
 import com.justin.springbootmall.rest.entity.PlainStudent;
+import com.justin.springbootmall.rest.exception.StudentErrorResponse;
+import com.justin.springbootmall.rest.exception.StudentNotFoundException;
 import com.justin.springbootmall.utils.StudentUtils;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +21,15 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException studentNotFoundException){
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(studentNotFoundException.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
+    }
+
     List<PlainStudent> plainStudents;
     // define @PostConstruct to load the student data only once when the given bean is constructed
     @PostConstruct
@@ -26,14 +40,17 @@ public class StudentRestController {
 
     @GetMapping("/students")
     public List<PlainStudent> getStudents(){
-//        List<PlainStudent> plainStudents = StudentUtils.createPlainStudents(5);
         return plainStudents;
 
     }
 
     @GetMapping("/students/{studentId}")
     public PlainStudent getStudent(@PathVariable int studentId){
-//        List<PlainStudent> plainStudents = StudentUtils.createPlainStudents(5);
+
+        if(studentId>=plainStudents.size() || studentId<0){
+            throw new StudentNotFoundException("student id not found, id:"+studentId);
+        }
+
         return plainStudents.get(studentId);
     }
 
