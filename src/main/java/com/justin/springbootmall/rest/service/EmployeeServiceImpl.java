fa4df3bd8d480dc700de3f98c45b235b31077e77
陳君豪 <justin.uob@gmail.com>
2023-10-30 14:54:02 +0800

    use jpa repo to replace entity manager

--- src/main/java/com/justin/springbootmall/rest/service/EmployeeServiceImpl.java
+++ src/main/java/com/justin/springbootmall/rest/service/EmployeeServiceImpl.java
@@ -1,7 +1,7 @@
 package com.justin.springbootmall.rest.service;
 
-import com.justin.springbootmall.rest.dao.EmployeeDAO;
 import com.justin.springbootmall.rest.entity.Employee;
+import com.justin.springbootmall.rest.jparepo.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,33 +11,34 @@ import java.util.List;
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-    private EmployeeDAO employeeDAO;
+//    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     @Transactional
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(Long id) {
-        return employeeDAO.findById(id);
+        return employeeRepository.findById(id).orElse(null);
     }
 
     @Override
     @Transactional
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
     @Override
     @Transactional
     public void deleteById(Long id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
